65e0c892dd2f944a562dcfb097c005dce1c17342
Aynaz Ansari <106579849+aynaz-ansari@users.noreply.github.com>
2025-06-02 10:51:31 +0330

    Delete Database directory

--- Database/query.sql
+++
@@ -1,3 +0,0 @@
-create Database products;
-use products;
-create table electronic_products(category varchar(10),brand varchar(10),model varchar(20),color varchar(10),price integer,warranty integer,total_price integer);
